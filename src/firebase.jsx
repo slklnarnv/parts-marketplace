b4a638b437ed34ce29be4cc98563ea6c8beae479
slkln@arnv <152384517+slklnarnv@users.noreply.github.com>
2026-04-22 15:51:08 +0000

    keys

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -4,19 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB4qmLmXayCWbGK43RNcZ0Q0KEY57nIX7w",
-
-  authDomain: "parts-marketplace.firebaseapp.com",
-
-  projectId: "parts-marketplace",
-
-  storageBucket: "parts-marketplace.firebasestorage.app",
-
-  messagingSenderId: "107879344610",
-
-  appId: "1:107879344610:web:ed63f09a821573d02631bb",
-
-  measurementId: "G-GVX0DF44DD",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
